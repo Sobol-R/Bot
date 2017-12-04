544a3fafb2deb0e503aedc24c7c533daeb4f89c4
Banana Android Z <ndrdbnnz@gmail.com>
2017-12-04 19:01:15 +0300

    02

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -1,8 +1,26 @@
 import model.*;
 
+import java.util.List;
+
 public final class MyStrategy implements Strategy {
+    Vehicles vehicles;
     @Override
     public void move(Player me, World world, Game game, Move move) {
+        if (world.getTickIndex() == 0) {
+            initialize();
+        }
+        vehicles.add(world.getNewVehicles());
+        vehicles.addUpdates(world.getVehicleUpdates());
+        if (world.getTickIndex() <= 1) {
+            sendUnitsToFacilities(world, move);
+        }
+    }
+    void initialize() {
+        vehicles = new Vehicles();
+    }
+    void sendUnitsToFacilities(World world, Move move) {
+        List<Facility> facilities = world.getSortedFacilities(0, 0);
+        Facility bs = facilities.get(0);
         if (world.getTickIndex() == 0) {
             move.setAction(ActionType.CLEAR_AND_SELECT);
             move.setRight(world.getWidth());
@@ -12,8 +30,9 @@ public final class MyStrategy implements Strategy {
 
         if (world.getTickIndex() == 1) {
             move.setAction(ActionType.MOVE);
-            move.setX(world.getWidth() / 2.0D);
-            move.setY(world.getHeight() / 2.0D);
+            move.setX(bs.getLeft());
+            move.setY(bs.getTop());
         }
     }
+
 }
